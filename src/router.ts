19ccf2edb3ce3850180e3219be272d4689d340b6
Alejandro de Jesús Escareño Flores <alejandroef04@gmail.com>
2025-01-31 08:59:09 -0600

    🧱 Models and handlers
    Agregando validacion para registro de usuarios

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,15 @@
-import { Router } from "express";
-import User from "./models/User";
+import { Router } from 'express';
+import { createAccount } from "./handlers";
+import { body } from 'express-validator';
 
 const router = Router();
 
-router.post('/auth/register', async (req, res) => {
-    await User.create(req.body);
-});
+router.post('/auth/register',
+    body('handle').notEmpty().withMessage('El handle es obligatorio'),
+    body('name').notEmpty().withMessage('El nombre es obligatorio'),
+    body('email').isEmail().withMessage('El email no es válido'),
+    body('password').notEmpty().withMessage('La contraseña es obligatoria'),
+    body('password').isLength({ min: 6 }).withMessage('La contraseña debe tener al menos 6 caracteres'),
+    createAccount);
 
 export default router;
