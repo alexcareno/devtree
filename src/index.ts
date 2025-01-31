0e7adaefb9ce9b1c54e32873d2b0aa04e439f6ea
Alejandro de Jesús Escareño Flores <alejandroef04@gmail.com>
2025-01-31 06:34:03 -0600

    colors

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ import app from "./server";
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-    console.log(colors.cyan(`Server is running on port ${port}`));
+    console.log(colors.bold.cyan(`Server is running on port ${port}`));
 });
